1d593c8c0413de3e7c75ad7646b323097037c74c
cobileacd <cobileacd@gmail.com>
2021-01-21 16:54:23 +0000

    bug de overflow na client_dash.

--- mas/login.js
+++ mas/login.js
@@ -7,9 +7,22 @@ function validate() {
 
     for(var acc in validAccounts) {
         var password = validAccounts[acc];
-        if (_mail == acc && _password == password) return true;
+        if (_mail == acc && _password == password) {
+            displayError("accError", false);
+            return true;
+        }
     }
 
-    alert("Email e/ou palavra-passe errados!");
+    displayError("accError", true);
     return false;
+}
+
+function displayError(errorName, display) {
+    if (display) {
+        document.getElementById(errorName).classList.remove("d-none");
+        document.getElementById(errorName).classList.add("d-block");
+    } else {
+        document.getElementById(errorName).classList.remove("d-block");
+        document.getElementById(errorName).classList.add("d-none");
+    }
 }
